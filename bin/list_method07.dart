7a5853974f201c9c15928f63558781bf4b1ccc91
ogabek <norpulatovogabek42@gmail.com>
2022-07-16 11:09:38 +0500

    task7

--- bin/list_method07.dart
+++ bin/list_method07.dart
@@ -5,6 +5,21 @@
         list01(list): parameter
     Returns:
         int: return answer
-*/
 
-void main() {}
+*/
+int func(List a) {
+  int q = 0;
+  int y = 0;
+  int s = a.length - 1;
+  while (q < s) {
+    if (a.contains(0)) {
+      y++;
+    }
+    q++;
+  }
+  return q;
+}
+
+void main() {
+  print(func([0, 0, 0, 0, 1, 1, 0]));
+}
