c29d05c3bd92d2b9e239075cdf10e058fae98b4d
ogabek <norpulatovogabek42@gmail.com>
2022-07-16 11:04:00 +0500

    task6

--- bin/list_method06.dart
+++ bin/list_method06.dart
@@ -6,5 +6,21 @@
     Returns:
         int: return answer
 */
+int func(List a) {
+  int r = 0;
+  int q = 0;
+  int y = a.length - 1;
+  //q = a.indexOf('apple');
+  while (y > r) {
+    //a.remove(r);
+    if (a.contains('apple')) {
+      q++;
+    }
+    r++;
+  }
+  return q;
+}
 
-void main() {}
+void main() {
+  print(func(['apple', 'apple', "orange", 'apple']));
+}
