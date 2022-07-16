7cff27143b3235a518a5c114087e119ba5282e0d
ogabek <norpulatovogabek42@gmail.com>
2022-07-16 14:04:30 +0500

    task10

--- bin/list_method10.dart
+++ bin/list_method10.dart
@@ -6,5 +6,25 @@
     Returns:
         list: return answer
 */
+List func(List a) {
+  int q = 0;
+  int m = 0;
+  int y = 0;
+  List s = [];
+  int w = a.length - 1;
+  while (q <= w) {
+    if (a[q] == 0) {
+      y++;
+    } else {
+      m++;
+    }
+    q++;
+  }
 
-void main() {}
+  s.addAll([m, y]);
+  return s;
+}
+
+void main() {
+  print(func([0, 0, 1, 1, 1, 1, 0]));
+}
